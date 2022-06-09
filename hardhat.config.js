f6efdf3174b6701f548255cb807b71f89982c8e4
Michael Burke <michael.dburke56@gmail.com>
2022-06-09 17:00:24 -0400

    Moved away from json env vars to dotenv npm package.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,14 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- require('@nomiclabs/hardhat-ethers');
- require('hardhat-gas-reporter');
- require('solidity-coverage');
- const { alchemyApiKey, mnemonic } = require('./.secrets.json');
- 
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-gas-reporter');
+require('solidity-coverage');
+require('dotenv').config();
+
+const alchemyApiKey = process.env.ALCHEMY_API_KEY;
+const mnemonic = process.env.MNEMONIC;
+
   module.exports = {
    solidity: "0.8.14",
    networks: {
