905fa6eabaeb6ab7d8e6715932418bfc8df1e655
MB <michael.dburke56@gmail.com>
2022-06-10 11:36:50 -0500

    Comment network config(requiring env var setup.

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,10 +11,12 @@ const mnemonic = process.env.MNEMONIC;
 
   module.exports = {
    solidity: "0.8.14",
+      /*
    networks: {
      rinkeby: {
        url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
        accounts: { mnemonic: mnemonic },
      },
    },
+   */
  };
